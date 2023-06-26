b08f43125a2c2ed83a386985961c4bbc2e9a9563
์์NNJio <phatsakorn.newz@gmail.com>
2023-06-26 19:14:28 +0700

    [Add] Default parameter

--- src/ep1.js
+++ src/ep1.js
@@ -1,32 +1,8 @@
-//! OLD
-// const colors = ["green", "red", "yallow"];
-// const green = colors[0];
-// const red = colors[1];
-// const yallow = colors[2];
-//! Destructuring
-// const [green, red, yallow] = colors;
-// const [, , yallow] = colors;
-// const [green, ,] = colors;
-// const [,red ,] = colors;
-
-// console.log(green);
-// console.log(red);
-// console.log(yallow);
-const product = {
-  productName: "Conputer",
-  price: 3000,
-  stock: 10,
+let getDataCustomer = (customerName = "Null", customerAddress = "Null") => {
+  const address = `Customer Name: ${customerName}
+    Address: ${customerAddress}`;
+  return address;
 };
-//! old
-// const productName = product.productName;
-// const price = product.price;
-// const stock = product.stock;
 
-//!Destructuring
-
-const { productName, price, stock } = product;
-
-console.log(product);
-console.log(productName);
-console.log(price);
-console.log(stock);
+console.log(getDataCustomer("Bankok"));
+console.log(getDataCustomer("Naneee"));
