4a75c50492da935caf46d7832fc61eae7eb8e4bc
์์NNJio <phatsakorn.newz@gmail.com>
2023-06-26 19:04:48 +0700

    [Add] Destructuring

--- src/ep1.js
+++ src/ep1.js
@@ -1,11 +1,32 @@
-// summation = (x, y) => x + y;
-// summation = (x, y, z) => x + y + z;
-// const summation = (x, y, z) => (z != undefined ? x + y + z : x + y);
-const summation = (...numberArr) => {
-  let total = 0;
-  for (let number of numberArr) total += number;
-  return total;
-};
+//! OLD
+// const colors = ["green", "red", "yallow"];
+// const green = colors[0];
+// const red = colors[1];
+// const yallow = colors[2];
+//! Destructuring
+// const [green, red, yallow] = colors;
+// const [, , yallow] = colors;
+// const [green, ,] = colors;
+// const [,red ,] = colors;
 
-// console.log(summation(50, 100));
-console.log(summation(500, 100, 50, 400, 50, 100, 250, 50));
+// console.log(green);
+// console.log(red);
+// console.log(yallow);
+const product = {
+  productName: "Conputer",
+  price: 3000,
+  stock: 10,
+};
+//! old
+// const productName = product.productName;
+// const price = product.price;
+// const stock = product.stock;
+
+//!Destructuring
+
+const { productName, price, stock } = product;
+
+console.log(product);
+console.log(productName);
+console.log(price);
+console.log(stock);
