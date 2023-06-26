2ac3d4f30c2b132298584923f5691665fdacdf0b
์์NNJio <phatsakorn.newz@gmail.com>
2023-06-26 18:11:26 +0700

    [Add] Objects

--- src/ep1.js
+++ src/ep1.js
@@ -1,4 +1,30 @@
-let fullNme = (fName, lName) => fName + lName;
-let setAge = (age) => "age = " + age;
-console.log(fullNme("New", "NNJio"));
-console.log(setAge(20));
+// Old JS
+const customer = {
+  customerName: "Phatsakorn",
+  age: 28,
+  address: "Bangkok",
+};
+console.log(customer);
+
+// New JS
+const username = "NNJio";
+const age = 28;
+const address = "Samutpakan";
+const newCustomer = {
+  username,
+  age,
+  address,
+};
+console.log(newCustomer);
+
+// Method
+
+const methodCustomer = {
+  username,
+  age,
+  address,
+  showData() {
+    customerNames: "John Doe", console.log("User Info: " + username);
+  },
+};
+methodCustomer.showData();
