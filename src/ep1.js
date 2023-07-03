0f54fd04ab42e0043517eba0a871b95b32fcc24e
์์NNJio <phatsakorn.newz@gmail.com>
2023-07-03 18:19:39 +0700

    [Add] Array Map

--- src/ep1.js
+++ src/ep1.js
@@ -1,9 +1,29 @@
-const colors = ["red", "green", "blue", "puple", "white"];
-console.log(colors);
-//! indexOf
-// const i = colors.indexOf("white");
-// console.log(i);
-//! find
-// const i = colors.find(e=>e=='blue');
-const i = colors.findIndex((e) => e == "blue");
-console.log(i);
+//! Map number
+// const numbers = [10, 20, 30, 40];
+// console.log(`Array Number = ${numbers}`);
+// const result = numbers.map((e) => e * 2);
+// console.log(`Array Map = ${result}`);
+//! Map String
+// const data = [
+//   "rain",
+//   "sun",
+//   "storm",
+//   "very sun",
+//   "hard rain",
+//   "good",
+//   "cloudy",
+// ];
+
+// console.log(`All day Weater = ${data}`);
+// const result = data.map((e, i) => {
+//   return `Day ${i + 1} weater is ${e}`;
+// });
+// console.log(result);
+//! Map Objects
+const data = [
+  { day: "01/06/2564", weather: "Sun", tem: 37 },
+  { day: "02/06/2564", weather: "Rain", tem: 27 },
+  { day: "03/06/2564", weather: "Cloudy", tem: 22 },
+];
+const result = data.map((e) => e.weather);
+console.log(result);
