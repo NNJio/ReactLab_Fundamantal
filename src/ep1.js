8a9580fb78a890951a5afe7f3049833ca0187072
์์NNJio <phatsakorn.newz@gmail.com>
2023-07-03 17:07:48 +0700

    [Add] Edit array

--- src/ep1.js
+++ src/ep1.js
@@ -1,12 +1,28 @@
-const data = [100, 200, 300];
-const data2 = [400, 500];
 
-// console.log(data);
-//! join
-// const result = data.join()
-// const result = data.join("*");
-// console.log(result);
-// console.log(data.join());
-//! concat
-const allData = data.concat(data2)
-console.log(allData);
+const data = [10,20,30,40,50]
+console.log(data)
+//! push
+data.push(...[500,1000,2000])
+data.push(1000)
+console.log(data)
+//! pop
+data.pop()
+data.pop()
+console.log(data)
+//! shift
+data.shift()
+data.shift()
+console.log(data)
+//! unshift
+data.unshift(999)
+data.unshift(5000)
+console.log(data)
+//! splice
+data.splice(1,2,999)
+console.log(data)
+//! slice
+data.slice(1,3)
+const lastData = data.slice(1,3)
+console.log(lastData)
+
+
