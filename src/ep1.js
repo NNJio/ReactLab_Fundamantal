3abc6b4ccd7d86d3e21ba0bc8e371e42116b79c5
์์NNJio <phatsakorn.newz@gmail.com>
2023-07-03 17:29:02 +0700

    [Add] Loop in array

--- src/ep1.js
+++ src/ep1.js
@@ -1,28 +1,27 @@
+const data = [10, 20, 30, 40, 50];
+// console.log(data[0])
+// console.log(data[1])
+// console.log(data[2])
+// console.log(data[3])
+// console.log(data[4])
+//! Loop For
+// for (let i = 0; i < data.length; i++) {
+//   if (data[i] >= 40) break;
+//   console.log(`Count Number ${i + 1} = ${data[i]}`);
+// }
+//! Loop ForEach
+let sum = 0;
+data.forEach((e) => {
+  sum += e;
+  console.log(`Summary = ${sum}`);
 
-const data = [10,20,30,40,50]
-console.log(data)
-//! push
-data.push(...[500,1000,2000])
-data.push(1000)
-console.log(data)
-//! pop
-data.pop()
-data.pop()
-console.log(data)
-//! shift
-data.shift()
-data.shift()
-console.log(data)
-//! unshift
-data.unshift(999)
-data.unshift(5000)
-console.log(data)
-//! splice
-data.splice(1,2,999)
-console.log(data)
-//! slice
-data.slice(1,3)
-const lastData = data.slice(1,3)
-console.log(lastData)
-
-
+  // if(e>=30){
+  //     console.log('Hello')
+  // }
+  console.log(`Number in Array data = ${e}`);
+});
+//! for of (Same Loop ForEach)
+for (const e of data) {
+  if (e >= 30) break;
+  console.log(`Array number data = ${e}`);
+}
