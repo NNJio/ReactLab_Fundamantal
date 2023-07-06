39358bd31df3f170d6e73225bd3f50eea00f09ae
์์NNJio <phatsakorn.newz@gmail.com>
2023-07-06 19:58:48 +0700

    [Add] Key Props UUID

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,20 +1,21 @@
 import Item from "./Item";
 import "./transaction.css";
+import { v4 as uuidv4 } from "uuid";
 
 const Transaction = () => {
   const data = [
-    {id:1, title: "Hopitel Bill", amount: "- 2000" },
-    {id:2, title: "Salary", amount: "+ 50000" },
-    {id:3, title: "Tarvil", amount: "- 500" },
-    {id:3, title: "Rent Room", amount: "- 8000" },
-    {id:4, title: "Pay Insurance", amount: "- 300" },
-    {id:5, title: "Pay Internet", amount: "- 200" },
+    { title: "Hopitel Bill", amount: "- 2000" },
+    { title: "Salary", amount: "+ 50000" },
+    { title: "Tarvil", amount: "- 500" },
+    { title: "Rent Room", amount: "- 8000" },
+    { title: "Pay Insurance", amount: "- 300" },
+    { title: "Pay Internet", amount: "- 200" },
   ];
   return (
     <ul className="item-list">
       {data.map((e) => (
         // <Item title={e.title} amount={e.amount} />
-        <Item {...e} key ={e.id}/>
+        <Item {...e} key={uuidv4()}/>
       ))}
     </ul>
   );
