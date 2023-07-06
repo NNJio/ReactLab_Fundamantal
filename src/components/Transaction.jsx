4c85b0196e35bae0a5d75bdf2294f8c9ed8124bd
์์NNJio <phatsakorn.newz@gmail.com>
2023-07-06 19:44:02 +0700

    [Add] Props array & map

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -7,13 +7,15 @@ const Transaction = () => {
     { title: "Salary", amount: "+ 50000" },
     { title: "Tarvil", amount: "- 500" },
     { title: "Rent Room", amount: "- 8000" },
+    { title: "Pay Insurance", amount: "- 300" },
+    { title: "Pay Internet", amount: "- 200" },
+
   ];
   return (
     <ul className="item-list">
-      <Item title={data[0].title} amount={data[0].amount} />
-      <Item title={data[1].title} amount={data[1].amount} />
-      <Item title={data[2].title} amount={data[2].amount} />
-      <Item title={data[3].title} amount={data[3].amount} />
+      {data.map((e) => (
+        <Item title={e.title} amount={e.amount} />
+      ))}
     </ul>
   );
 };
