2705bddaa7511d3588c571247771a4e8740be542
์์NNJio <phatsakorn.newz@gmail.com>
2023-07-06 19:46:54 +0700

    [Add] Props & Spread operator

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -9,12 +9,12 @@ const Transaction = () => {
     { title: "Rent Room", amount: "- 8000" },
     { title: "Pay Insurance", amount: "- 300" },
     { title: "Pay Internet", amount: "- 200" },
-
   ];
   return (
     <ul className="item-list">
       {data.map((e) => (
-        <Item title={e.title} amount={e.amount} />
+        // <Item title={e.title} amount={e.amount} />
+        <Item {...e} />
       ))}
     </ul>
   );
