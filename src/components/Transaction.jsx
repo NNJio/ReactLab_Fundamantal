4148e2e5b9a57d29d95a0e15d3e5b1c063405349
์์NNJio <phatsakorn.newz@gmail.com>
2023-07-06 19:32:30 +0700

    [Add] Props array data

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -2,14 +2,18 @@ import Item from "./Item";
 import "./transaction.css";
 
 const Transaction = () => {
+  const data = [
+    { title: "Hopitel Bill", amount: "- 2000" },
+    { title: "Salary", amount: "+ 50000" },
+    { title: "Tarvil", amount: "- 500" },
+    { title: "Rent Room", amount: "- 8000" },
+  ];
   return (
     <ul className="item-list">
-      <Item title="Hopitel Bill" amount="- 2000" />
-      <Item title="Salary" amount="+ 50000" />
-      <Item title="Tarvil" amount="- 500" />
-      <Item title="Rent Room" amount="+ 8000" />
-
-
+      <Item title={data[0].title} amount={data[0].amount} />
+      <Item title={data[1].title} amount={data[1].amount} />
+      <Item title={data[2].title} amount={data[2].amount} />
+      <Item title={data[3].title} amount={data[3].amount} />
     </ul>
   );
 };
