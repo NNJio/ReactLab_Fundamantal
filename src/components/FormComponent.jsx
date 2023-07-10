44663194a0ecdce751efb31cff13924cc866b212
์์NNJio <phatsakorn.newz@gmail.com>
2023-07-10 14:23:07 +0700

    [Add] Event

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,18 +1,34 @@
 import "./FormComponent.css";
 const FormComponent = () => {
+  const inputTitle = (event) => {
+    console.log(event.target.value);
+  };
+  const inputAmount = (event) => {
+    console.log(event.target.value);
+  };
+  const saveItem = (event) => {
+    event.preventDefault();
+    console.log("Success");
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={saveItem}>
         <div className="form-control">
-          <label>Topic</label>
-          <input type="Text" placeholder="Input Topic" />
+          <label>Title</label>
+          <input type="Text" placeholder="Input Title" onChange={inputTitle} />
         </div>
         <div className="form-control">
-          <label>Currency value</label>
-          <input type="number" placeholder="(+ Income , - Pay)" />
+          <label>Amount value</label>
+          <input
+            type="number"
+            placeholder="(+ Income , - Pay)"
+            onChange={inputAmount}
+          />
         </div>
         <div>
-          <button type="submit" className="btn" >Done</button>
+          <button type="submit" className="btn">
+            Done
+          </button>
         </div>
       </form>
     </div>
